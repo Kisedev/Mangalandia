9bf296e3e1960f9edecefb6f5ce831cad9593424
Kise <ruosena@gmail.com>
2020-02-09 17:47:53 -0300

    home page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('/catalogo');
+  res.render('home', {title: 'Mangalandia'})
 });
 
 module.exports = router;
