1b55b0ec9c14d5fbb35c8905d013cb2181cb95e3
Kise <ruosena@gmail.com>
2020-02-23 11:56:59 -0300

    not draggable page

--- public/scripts/leitor.js
+++ public/scripts/leitor.js
@@ -1,20 +1,21 @@
-
 $(document).ready(function() {
   //initialize swiper when document ready
   var Leitor = new Swiper(".swiper-container", {
     // Optional parameters
     slidesPerViews: 1,
-    direction: 'horizontal',
+    direction: "horizontal",
+    noSwiping: true,
+    noSwipingClass: "swiper-not",
+    noSwipingSelector: "img",
     loop: false,
-
+    grabCursor: true,
     keyboard: {
-        enabled: true
+      enabled: true
     },
     scrollbar: {
-        el: '.swiper-scrollbar',
-        hide: true
+      el: ".swiper-scrollbar",
+      hide: true
     }
   });
-  var Leitor = document.querySelector('.swiper-container').swiper
+  var Leitor = document.querySelector(".swiper-container").swiper;
 });
-
