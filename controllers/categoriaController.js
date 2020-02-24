fdf74963a392c4e8e66e6baf9c255a0bfdbcacc5
Kise <ruosena@gmail.com>
2020-02-24 10:27:06 -0300

    categoria update

--- controllers/categoriaController.js
+++ controllers/categoriaController.js
@@ -1,125 +1,210 @@
-var categoria = require('../models/categoria');
-var manga = require('../models/manga');
+var categoria = require("../models/categoria");
+var manga = require("../models/manga");
 
-const async = require('async');
-const validator = require('express-validator');
+const async = require("async");
+const validator = require("express-validator");
 
-exports.categoria_lista = function (req, res) {
-    categoria.find({})
-        .exec((err, result) => {
-            if (err) { return next(err) };
-            res.render('categorias', { title: 'Categorias & Gêneros', categorias: result })
-        })
-};
-
-exports.categoria_info = function (req, res) {
-    async.parallel({
-        categoria: function(callback) {
-            categoria.findById(req.params.id)
-            .exec(callback)
-        },
-        categoria_mangas: function(callback) {
-            manga.find({'categoria': req.params.id})
-            .exec(callback)
-        }
-    }, (err, results) => {
-        if(err) {next(err)};
-        res.render('categoria', {title: results.categoria.nome, categoria: results.categoria, categoria_mangas: results.categoria_mangas})
+exports.categoria_lista = function(req, res, next) {
+  categoria.find({}).exec((err, result) => {
+    if (err) {
+      return next(err);
+    }
+    res.render("categorias", {
+      title: "Categorias & Gêneros",
+      categorias: result
     });
+  });
 };
 
-exports.categoria_add_get = function (req, res) {
-    res.render('forms/categoria', { title: 'Adicionar Categoria' })
+exports.categoria_info = function(req, res, next) {
+  async.parallel(
+    {
+      categoria: function(callback) {
+        categoria.findById(req.params.id).exec(callback);
+      },
+      categoria_mangas: function(callback) {
+        manga.find({ categoria: req.params.id }).exec(callback);
+      }
+    },
+    (err, results) => {
+      if (err) {
+        next(err);
+      }
+      res.render("categoria", {
+        title: results.categoria.nome,
+        categoria: results.categoria,
+        categoria_mangas: results.categoria_mangas
+      });
+    }
+  );
+};
+
+exports.categoria_add_get = function(req, res, next) {
+  res.render("forms/categoria", { title: "Adicionar Categoria" });
 };
 
 exports.categoria_add_post = [
-    // lista de middlewares para executar assicronamente
-    validator.body('nome', 'O Nome deve ter pelo menos 3 caracteres').isLength({ min: 3 }).trim(),
-    // valida campo nome do form
-    validator.sanitizeBody('nome').escape(),
-    // anti cross script attack de tags html ou cod js 
-    (req, res, next) => {
-        const errors = validator.validationResult(req);
+  // lista de middlewares para executar assicronamente
+  validator
+    .body("nome", "O Nome deve ter pelo menos 3 caracteres")
+    .isLength({ min: 3, max: 30 })
+    .trim(),
+  // valida campo nome do form
+  validator.sanitizeBody("nome").escape(),
+  // anti cross script attack de tags html ou cod js
+  (req, res, next) => {
+    const errors = validator.validationResult(req);
 
-        let cat = new categoria(
-            { nome: req.body.nome }
-        );
+    let cat = new categoria({ nome: req.body.nome });
 
-        if (!errors.isEmpty()) {
-            // ha erros ent renderiza novamente com os dados e alerta 
-            res.render('forms/categoria', { title: 'Adicionar Categoria', categoria: cat, errors: errors.array() });
-            return;
-        } else {
-            // os dados estao corretos
-            categoria.findOne({ nome: req.body.nome })
-                .exec((err, cat_encontrada) => {
-                    if (err) { return next(err) };
-
-                    if (cat_encontrada) {
-                        // vai pra pagina do genero que ja existia 
-                        res.redirect(cat_encontrada.url);
-                    } else {
-                        cat.save(function (err) {
-                            if (err) { return next(err) };
-                            // salvou vai pra pagina
-                            res.redirect(cat.url);
-                        });
-                    }
-                })
+    if (!errors.isEmpty()) {
+      // ha erros ent renderiza novamente com os dados e alerta
+      res.render("forms/categoria", {
+        title: "Adicionar Categoria",
+        categoria: cat,
+        errors: errors.array()
+      });
+      return;
+    } else {
+      // os dados estao corretos
+      categoria.find({ nome: req.body.nome }).exec((err, cat_encontrada) => {
+        if (err) {
+          return next(err);
         }
+
+        if (cat_encontrada) {
+          // vai pra pagina do genero que ja existia
+          res.redirect(cat_encontrada.url);
+        } else {
+          cat.save(function(err) {
+            if (err) {
+              return next(err);
+            }
+            // salvou vai pra pagina
+            res.redirect(cat.url);
+          });
+        }
+      });
     }
+  }
 ];
 
+exports.categoria_rm_get = function(req, res, next) {
+  async.parallel(
+    {
+      categoria: function(callback) {
+        categoria.findById(req.params.id).exec(callback);
+      },
+      categoria_mangas: function(callback) {
+        manga.find({ categoria: req.params.id }).exec(callback);
+      }
+    },
+    (err, results) => {
+      if (err) {
+        next(err);
+      }
+      if (!results.categoria) {
+        res.redirect("/catalogo/categorias");
+      }
 
-exports.categoria_rm_get = function (req, res) {
-    async.parallel({
-        categoria: function(callback) {
-            categoria.findById(req.params.id)
-            .exec(callback);
-        },
-        categoria_mangas: function(callback) {
-            manga.find({'categoria': req.params.id})
-            .exec(callback)
-        }
-    }, (err, results) => {
-        if (err) {next(err)}; 
-        if(!results.categoria) {
-            res.redirect('/catalogo/categorias');
-        }
+      res.render("forms/categoria_rm", {
+        title: "Remover Categoria",
+        categoria: results.categoria,
+        categoria_mangas: results.categoria_mangas
+      });
+    }
+  );
+};
 
-        res.render('forms/categoria_rm', {title: 'Remover Categoria', categoria: results.categoria, categoria_mangas: results.categoria_mangas})
+exports.categoria_rm_post = function(req, res, next) {
+  async.parallel(
+    {
+      categoria: function(callback) {
+        categoria.findById(req.body.cat_id).exec(callback);
+      },
+      categoria_mangas: function(callback) {
+        manga.find({ categoria: req.body.cat_id }).exec(callback);
+      }
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.categoria_mangas.length > 0) {
+        res.render("forms/categoria_rm", {
+          title: "Remover Categoria",
+          categoria: results.categoria,
+          categoria_mangas: results.categoria_mangas
+        });
+        return;
+      } else {
+        categoria.findByIdAndRemove(req.body.cat_id, function removerCategoria(err) {
+          if (err) {
+            return next(err);
+          }
+          res.redirect("/catalogo/categorias");
+        });
+      }
+    }
+  );
+};
+
+exports.categoria_att_get = function(req, res, next) {
+  categoria.findById(req.params.id).exec((err, cat) => {
+    if (err) {
+      return next(err);
+    }
+    res.render("forms/categoria", {
+      title: "Atualizar Categoria",
+      categoria: cat
     });
+  });
 };
 
-exports.categoria_rm_post = function (req, res) {
-    async.parallel({
-        categoria: function(callback) {
-            categoria.findById(req.body.cat_id)
-            .exec(callback);
-        },
-        categoria_mangas: function(callback) {
-            manga.find({'categoria': req.body.cat_id})
-            .exec(callback);
+exports.categoria_att_post = [
+  validator
+    .body("nome", "O Nome deve ter pelo menos 3 caracteres")
+    .isLength({ min: 3, max: 30 })
+    .trim(),
+  validator.sanitizeBody("nome").escape(),
+
+  (req, res, next) => {
+    const errors = validator.validationResult(req);
+
+    let cat_editada = new categoria({
+      nome: req.body.nome,
+      _id: req.params.id
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("forms/categoria", {
+        title: "Atualizar Categoria",
+        categoria: cat_editada,
+        errors: errors.array()
+      });
+      return;
+    } else {
+      categoria.find({ nome: req.body.nome }).exec((err, cat_encontrada) => {
+        console.log('cat_encontrada', cat_encontrada, cat_encontrada.url)
+        if (err) {
+          return next(err);
         }
-    }, (err, results) => {
-        if (err) {return next(err)}
-        if(results.categoria_mangas.length > 0) {
-            res.render('forms/categoria_rm', {title: 'Remover Categoria', categoria: results.categoria, categoria_mangas: results.categoria_mangas})
-            return;
+        if (cat_encontrada.url) {
+          res.redirect(cat_encontrada.url);
         } else {
-            categoria.findByIdAndRemove(req.body.cat_id, function removerCategoria(err) {
-                if (err) {return next(err)}
-                
-                res.redirect('/catalogo/categorias');
-            })
+          categoria.findByIdAndUpdate(
+            req.params.id,
+            cat_editada,
+            (err, cat_atualizada) => {
+              if (err) {
+                return next(err);
+              }
+              console.log('cat_atualizada', cat_atualizada, cat_atualizada.url);
+              res.redirect(cat_atualizada.url);
+            }
+          );
         }
-    })
-};
-
-exports.categoria_att_get = function (req, res) {
-    res.send('DX O HOMEM TRABAIA: atualizar categoria GET');
-};
-
-exports.categoria_att_post = function (req, res) {
-    res.send('DX O HOMEM TRABAIA: atualizar categoria POST');
-};
+      });
+    }
+  }
+];
--- controllers/mangacapController.js
+++ controllers/mangacapController.js
@@ -1,151 +1,208 @@
-var manga = require('../models/manga');
-var mangacap = require('../models/mangacap');
+var manga = require("../models/manga");
+var mangacap = require("../models/mangacap");
 
-const { body,validationResult } = require('express-validator/check');
-const { sanitizeBody } = require('express-validator/filter');
+const { body, validationResult } = require("express-validator/check");
+const { sanitizeBody } = require("express-validator/filter");
 
-// exibe lista com todos caps 
+// exibe lista com todos caps
 exports.mangacap_lista = function(req, res, next) {
-    manga.findById(req.params.manga_id)
-    .populate('capitulo')
+  manga
+    .findById(req.params.manga_id)
+    .populate("capitulo")
     .exec((err, manga) => {
-        if (err) {return next(err)};
-        res.render('capitulos', { title: 'Todos Capítulos', manga});
-    })
+      if (err) {
+        return next(err);
+      }
+      res.render("capitulos", { title: "Todos Capítulos", manga });
+    });
 };
 
 exports.mangacap_ler = function(req, res, next) {
-    // encontra capitulo diretamente e preenche dados do manga 
-    mangacap.findById(req.params.id)
-    .populate('manga')
+  // encontra capitulo diretamente e preenche dados do manga
+  mangacap
+    .findById(req.params.id)
+    .populate("manga")
     .exec((err, cap) => {
-        // se der erro ao buscar
-        if (err) {return next(err)};
-        // se nao tiver encontrado ao ter buscado
-        if (!cap) {
-            var err = new Error('Capítulo não encontrado');
-            err.status = 404;
-            return next(err);
-        }
-        // achou o cap
-        res.render('leitor', {title: cap.titulo, cap});
-    })
-}
+      // se der erro ao buscar
+      if (err) {
+        return next(err);
+      }
+      // se nao tiver encontrado ao ter buscado
+      if (!cap) {
+        var err = new Error("Capítulo não encontrado");
+        err.status = 404;
+        return next(err);
+      }
+      // achou o cap
+      res.render("leitor", { title: cap.titulo, cap });
+    });
+};
 
 exports.mangacap_add_get = function(req, res, next) {
-    manga.findById(req.params.manga_id)
-    .exec(function(err, manga) {
-        if (err) {next(err)}
-        res.render('forms/capitulo', {title: 'Adicionar Capítulo', manga})
-    })
-}
+  manga.findById(req.params.manga_id).exec(function(err, manga) {
+    if (err) {
+      next(err);
+    }
+    res.render("forms/capitulo", { title: "Adicionar Capítulo", manga });
+  });
+};
 
 exports.mangacap_add_post = [
-    body('isbn', 'ISBN é necessário').isLength({min: 1}).trim(),
-    body('num', 'Número do capítulo é necessário').isLength({min: 1}).trim(),
-    body('lancamento', 'Data de lançamento inválida').optional({checkFalsy: true}).isISO8601(),
+  body("isbn", "ISBN é necessário")
+    .isLength({ min: 1 })
+    .trim(),
+  body("num", "Número do capítulo é necessário")
+    .isLength({ min: 1 })
+    .trim(),
+  body("lancamento", "Data de lançamento inválida")
+    .optional({ checkFalsy: true })
+    .isISO8601(),
 
-    sanitizeBody('isbn').escape(),
-    sanitizeBody('num').escape(),
-    sanitizeBody('lancamento').toDate(),
-    sanitizeBody('nome').trim().escape(),
+  sanitizeBody("isbn").escape(),
+  sanitizeBody("num").escape(),
+  sanitizeBody("lancamento").toDate(),
+  sanitizeBody("nome")
+    .trim()
+    .escape(),
 
-    (req, res, next) => {
-        const errors = validationResult(req);
+  (req, res, next) => {
+    const errors = validationResult(req);
 
-        let cap = new mangacap({
-            manga: req.body.manga,
-            isbn: req.body.isbn,
-            num: req.body.num,
-            lancamento: req.body.lancamento,
-            nome: req.body.nome
-        });
+    let cap = new mangacap({
+      manga: req.body.manga,
+      isbn: req.body.isbn,
+      num: req.body.num,
+      lancamento: req.body.lancamento,
+      nome: req.body.nome
+    });
 
-        if(!errors.isEmpty()) {
-            manga.findById(req.body.manga)
-            .exec((err, manga) => {
-                if (err) {return next(err)}
-                res.render('forms/capitulo', {title: 'Adicionar Capítulo', errors: errors.array(), manga, capitulo: cap});
-            });
-            return;
-        } else {
-            cap.save(function(err) {
-                if (err) {return next(err)}
-                res.redirect(cap.url)
-            });
+    if (!errors.isEmpty()) {
+      manga.findById(req.body.manga).exec((err, manga) => {
+        if (err) {
+          return next(err);
         }
+        res.render("forms/capitulo", {
+          title: "Adicionar Capítulo",
+          errors: errors.array(),
+          manga,
+          capitulo: cap
+        });
+      });
+      return;
+    } else {
+      cap.save(function(err) {
+        if (err) {
+          return next(err);
+        }
+        res.redirect(cap.url);
+      });
     }
-]
+  }
+];
 
 exports.mangacap_rm_get = function(req, res, next) {
-    mangacap.findById(req.params.id)
-    .populate('manga')
+  mangacap
+    .findById(req.params.id)
+    .populate("manga")
     .exec((err, cap) => {
-        if (err) {return next(err)}
-        if(!cap) {
-            res.redirect(`/catalogo/manga/${req.params.manga_id}/capitulos`)
-        }
+      if (err) {
+        return next(err);
+      }
+      if (!cap) {
+        res.redirect(`/catalogo/manga/${req.params.manga_id}/capitulos`);
+      }
 
-        res.render('forms/capitulo_rm', {title: 'Remover Capítulo', cap})
-    })
-}
+      res.render("forms/capitulo_rm", { title: "Remover Capítulo", cap });
+    });
+};
 
 exports.mangacap_rm_post = function(req, res, next) {
-    mangacap.findById(req.body.cap_id)
-    .populate('manga')
+  mangacap
+    .findById(req.body.cap_id)
+    .populate("manga")
     .exec((err, cap) => {
-        if(err) {return next(err)}
-        mangacap.findByIdAndRemove(req.body.cap_id, function removerCap(err) {
-            if(err) {return next(err)}
-            res.redirect(`/catalogo/manga/${req.params.manga_id}/capitulos`)
-        })
-    })
-}
+      if (err) {
+        return next(err);
+      }
+      mangacap.findByIdAndRemove(req.body.cap_id, function removerCap(err) {
+        if (err) {
+          return next(err);
+        }
+        res.redirect(`/catalogo/manga/${req.params.manga_id}/capitulos`);
+      });
+    });
+};
 
 exports.mangacap_att_get = function(req, res, next) {
-    mangacap.findById(req.params.id)
-    .populate('manga')
+  mangacap
+    .findById(req.params.id)
+    .populate("manga")
     .exec(function(err, result) {
-        if (err) {next(err)}
-        res.render('forms/capitulo', {title: 'Atualizar Capítulo', capitulo: result, manga: result.manga})
-    })
-}
+      if (err) {
+        next(err);
+      }
+      res.render("forms/capitulo", {
+        title: "Atualizar Capítulo",
+        capitulo: result,
+        manga: result.manga
+      });
+    });
+};
 
 exports.mangacap_att_post = [
-    body('isbn', 'ISBN é necessário').isLength({min: 1}).trim(),
-    body('num', 'Número do capítulo é necessário').isLength({min: 1}).trim(),
-    body('lancamento', 'Data de lançamento inválida').optional({checkFalsy: true}).isISO8601(),
+  body("isbn", "ISBN é necessário")
+    .isLength({ min: 1 })
+    .trim(),
+  body("num", "Número do capítulo é necessário")
+    .isLength({ min: 1 })
+    .trim(),
+  body("lancamento", "Data de lançamento inválida")
+    .optional({ checkFalsy: true })
+    .isISO8601(),
 
-    sanitizeBody('isbn').escape(),
-    sanitizeBody('num').escape(),
-    sanitizeBody('lancamento').toDate(),
-    sanitizeBody('nome').trim().escape(),
+  sanitizeBody("isbn").escape(),
+  sanitizeBody("num").escape(),
+  sanitizeBody("lancamento").toDate(),
+  sanitizeBody("nome")
+    .trim()
+    .escape(),
 
-    (req, res, next) => {
-        const errors = validationResult(req);
+  (req, res, next) => {
+    const errors = validationResult(req);
 
-        let cap_editado = new mangacap({
-            manga: req.body.manga,
-            isbn: req.body.isbn,
-            num: req.body.num,
-            lancamento: req.body.lancamento,
-            nome: req.body.nome,
-            _id: req.params.id
-        });
+    let cap_editado = new mangacap({
+      manga: req.body.manga,
+      isbn: req.body.isbn,
+      num: req.body.num,
+      lancamento: req.body.lancamento,
+      nome: req.body.nome,
+      _id: req.params.id
+    });
 
-        if(!errors.isEmpty()) {
-            manga.findById(req.body.manga)
-            .exec((err, manga) => {
-                if (err) {return next(err)}
-                res.render('forms/capitulo', {title: 'Atualizar Capítulo', errors: errors.array(), manga, capitulo: cap_editado});
-            });
-            return;
-        } else {
-            mangacap.findByIdAndUpdate(req.params.id, cap_editado, (err, cap_atualizado) => {
-                if (err) {return next(err)}
-                res.redirect(cap_atualizado.url);
-            })
+    if (!errors.isEmpty()) {
+      manga.findById(req.body.manga).exec((err, manga) => {
+        if (err) {
+          return next(err);
         }
+        res.render("forms/capitulo", {
+          title: "Atualizar Capítulo",
+          errors: errors.array(),
+          manga,
+          capitulo: cap_editado
+        });
+      });
+      return;
+    } else {
+      mangacap.findByIdAndUpdate(
+        req.params.id,
+        cap_editado,
+        (err, cap_atualizado) => {
+          if (err) {
+            return next(err);
+          }
+          res.redirect(cap_atualizado.url);
+        }
+      );
     }
-
-]
+  }
+];
